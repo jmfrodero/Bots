e2aab6652680b0f0a8e7d599604114ce2377777b
Fernandez  Rodero <j.fernandez.rodero@accenture.com>
2020-01-07 16:45:09 +0100

    index

--- index.py
+++ index.py
@@ -1,1 +1,1 @@
-https://api.telegram.org/bot<601100535:AAHUTjnmy-HaWowm5S9ygrPRCB7tUYa6E2E>/setWebHook?url=<https://botnoquedar.herokuapp.com/>
+https://api.telegram.org/bot<601100535:AAHUTjnmy-HaWowm5S9ygrPRCB7tUYa6E2E>/setWebHook?url=https://botnoquedar.herokuapp.com/
