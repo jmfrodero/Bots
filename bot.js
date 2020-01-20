3b8402ec8dffa42d75fb77e37ebaa983b9a5a274
Fernandez  Rodero <j.fernandez.rodero@accenture.com>
2020-01-20 15:38:22 +0000

    Prueba

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ else {
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
 bot.on('message', (msg) => {
-  if (msg.search('quedar') != -1) {
+  if (msg.indexOf('quedar') !== -1) {
     bot.sendMessage(msg.chat.id,'Gon no puede quedar,sry').then(() => {
       });
   }
